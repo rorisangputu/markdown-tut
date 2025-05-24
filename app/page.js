a532b2ca4f5e769dca4e27f084a363c20d8dba05
Rorisang Putu <rorisangputu@gmail.com>
2025-05-24 22:11:33 +0200

    Add post metadata retrieval in Home component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import getPostMetadata from "@/utils/getPostMetadata";
+
 
 
 export default function Home() {
+  const postMetadata = getPostMetadata('recipes')
+  console.log(postMetadata)
   return (
     <main>
 
