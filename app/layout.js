be66bdcf9efafb30de303fde3abd1859599ef50a
Rorisang Putu <rorisangputu@gmail.com>
2025-05-24 21:16:14 +0200

    Wrap header title in Link component for improved navigation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +21,9 @@ export default function RootLayout({ children }) {
 
   let header = (
     <header>
-      <h1>Recipe App</h1>
+      <Link href={'/'}>
+        <h1>Recipe App</h1>
+      </Link>
     </header>
   )
 
