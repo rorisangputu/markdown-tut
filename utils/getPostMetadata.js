a44c14006cf6cc089cdd047991843cab33dcde76
Rorisang Putu <rorisangputu@gmail.com>
2025-05-24 21:59:59 +0200

    Fix getPostMetadata function to correctly read markdown file contents and return post metadata

--- utils/getPostMetadata.js
+++ utils/getPostMetadata.js
@@ -1,8 +1,24 @@
 import fs from 'fs'
 import matter from 'gray-matter'
+import { title } from 'process';
 
 function getPostMetadata(basePath) {
     const folder = basePath + '/'
     const files = fs.readdirSync(folder)
-    const markdownPosts = files.filter(file => file.endsWith('.md'))
+    const markdownPosts = files.filter(file => file.endsWith('.md'));
+
+    // get the file data
+
+    const post = markdownPosts.map(() => {
+        const fileContents = fs.readFileSync(`${basePath}/${filename}`,
+            'utf8'
+        )
+        const matterResult = matter(fileContents)
+        return {
+            title: matterResult.data.title,
+            prep_time: matterResult.data.prep_time,
+            cook_time: matterResult.data.cook_time,
+            bio: matterResult.data.description,
+        }
+    })
 }
