5fdd3169ce8c481add3a0e3a79b28b7135741af2
Rorisang Putu <rorisangputu@gmail.com>
2025-05-24 22:17:38 +0200

    Add post data rendering to PostCard component

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,10 +1,13 @@
 import Link from "next/link"
 
 
-const PostCard = () => {
+const PostCard = ({post}) => {
   return (
       <Link href={''}>
-        
+        <h1>{post.title}</h1>
+        <p>{post.bio}</p>
+        <p>Prep: {post.prep_time}</p>
+        <p>Cook Time: {post.cook_time}</p>
       </Link>
   )
 }
