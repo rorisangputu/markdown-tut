188e1ed2b6474fa650d7a73132c9e9ff58b62e3e
Rorisang Putu <rorisangputu@gmail.com>
2025-05-24 22:36:10 +0200

    Update PostCard component to correctly link to recipe pages and display cooking times

--- components/PostCard.js
+++ components/PostCard.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 const PostCard = (props) => {
     const {post} = props;
   return (
-      <Link className="unstyled" href={''}>
+      <Link className="unstyled" href={`/recipe/${post.slug}`}>
         <div className="postCard">
             <h3>{post.title}</h3>
             <p>{post.bio}</p>
@@ -12,6 +12,10 @@ const PostCard = (props) => {
                 <div>
                     <h5>Prep Time</h5>
                     <p>{post.prep_time}</p>
+                </div>
+                <div>
+                    <h5>Cook Time</h5>
+                    <p>{post.cook_time}</p>
                 </div>
             </div>
         </div>
